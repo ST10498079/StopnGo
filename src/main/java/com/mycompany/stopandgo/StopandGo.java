5e4d611cd9001e8e6fde70aa3b8a74995bc2f0b6
unknown <st10498079@myemeris.edu.za>
2026-02-26 08:33:55 +0200

    Final Submission

---
+++ src/main/java/com/mycompany/stopandgo/StopandGo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stopandgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class StopandGo {
+
+    public static void main(String[] args) {
+        // STOP and GO Signs (side by side)
+
+        System.out.println("\t    XXXXXXX                XXXXXXX");
+        System.out.println("\t  XXXXXXXXXXX            XXXXXXXXXXX");
+        System.out.println("\t XXXXX      XXXXX        XXXX     XXXX");
+        System.out.println("\t XXXXX STOP XXXXX       XXX   GO    XXX");
+        System.out.println("\t XXXXX      XXXXX        XXXX     XXXX");
+        System.out.println("\t  XXXXXXXXXXX            XXXXXXXXXXX");
+        System.out.println("\t    XXXXXXX                XXXXXXX");
+
+        // Poles
+        System.out.println("\t       XX                      XX");
+        System.out.println("\t       XX                      XX");
+        System.out.println("\t       XX                      XX");
+        System.out.println("\t       XX                      XX");
+
+        // Base
+        System.out.println("\t     XXXXXX                  XXXXXX");
+        System.out.println("\t   XXXXXXXXXX              XXXXXXXXXX");
+        ------------------------------------------------------------------
+    }
+}
